ac56af4c70cc00d48b8a794bff7a3acd2f6c61d2
jobpawat <pawat-ka@rmutp.ac.th>
2020-10-13 11:55:34 -0400

    initail commit

---
+++ config/firebaseConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAvqLuhI33JZIYxlgLlOdXE8lLwodPJgSk",
+    authDomain: "myshop-ea026.firebaseapp.com",
+    databaseURL: "https://myshop-ea026.firebaseio.com",
+    projectId: "myshop-ea026",
+    storageBucket: "myshop-ea026.appspot.com",
+    messagingSenderId: "289669505380",
+    appId: "1:289669505380:web:acefa5a017a886a99a5a71"
+};
+
+module.exports = firebaseConfig
---
+++ routes/index.js
@@ -0,0 +1,37 @@
+const { json } = require('express');
+const express = require('express');
+const generatePayload = require('promptpay-qr');
+const qrcode = require('qrcode');
+const fs = require('fs');
+const admin = require('firebase-admin');
+const app = require('../app');
+const svg64 = require('svg64');
+const firebase = require("firebase/app");
+require("firebase/auth");
+require("firebase/firestore");
+require("firebase/storage");
+const config =  require('../config/firebaseConfig');
+const serviceAccount = require("../config/servicekey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://myshop-ea026.firebaseio.com'
+});
+
+firebase.initializeApp(config)
+firebase.auth().signInWithEmailAndPassword("admin@admin.com","123456")
+  .catch(function(error){
+    console.log(error.message)
+  })
+
+const db = admin.firestore();
+var router = express.Router();
+
+router.get('/qr',function(req, res, next) {
+  var tel = req.query.tel
+  var amount = req.query.amount
+  const payload = generatePayload(tel, amount)
+  res.send(payload)
+})
+
+module.exports = router;
